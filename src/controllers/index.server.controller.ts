4c63f9d6d8fd5f2fe965d70229438901917db977
Hugh Ashby-Hayter <Hugh.Ashby-Hayter@Imperial.co.uk>
2022-05-01 12:42:43 +0100

    created weather endpoint

--- src/controllers/index.server.controller.ts
+++ src/controllers/index.server.controller.ts
@@ -3,6 +3,7 @@ import WeatherWorld from 'weather-world';
 import { database }  from '../database/index';
 
 export default class IndexController {
+  dateFormat = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
   constructor (private api: WeatherWorld) {
   }
 
@@ -16,6 +17,37 @@ export default class IndexController {
         res.render('index', { title: x.postcode });
       });
     });
+  }
+
+  public weather = (req: Request, res: Response, next: Function): void => {
+    if (req.query.start == undefined || req.query.end == undefined){
+      res.status(400).send();
+      return;
+    }
+    
+    if (!this.dateFormat.test(req.query.start.toString()) || !this.dateFormat.test(req.query.end.toString())){
+      res.status(400).send('Incorrect date format used, use yyyy-MM-dd.');
+      return;
+    }
+    
+    let start = new Date(req.query.start.toString());
+    let end = new Date(req.query.end.toString());
+    let earliestDate = new Date(2008, 6, 1);
+
+    if (start > end){
+      res.status(400).send('Start date can not be after date.');
+      return;
+    }
+
+    if (start < earliestDate){
+      res.status(400).send('Start date can not be before 2008-07-01');
+      return;
+    }
+
+    this.api.getWeatherData(req.params.postcode, start, end)
+      .then((x) => {
+        res.status(200).send(x);
+      });
   }
 
   public msg(req: Request, res: Response): void {
--- src/routes/index.server.route.ts
+++ src/routes/index.server.route.ts
@@ -7,6 +7,7 @@ export default class IndexRoute {
     let api = new WeatherWorld();
     let indexController = new IndexController(api);
     app.get('/', indexController.index)
+    app.get('/weather/:postcode', indexController.weather)
     app.get('/msg', indexController.msg)
   }
 }
